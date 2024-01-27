f5779752be07d7495d2b6d05c6b2a86f23975bb1
Ting-Way, Huang <0978wally@gmail.com>
2024-01-27 21:58:13 +0800

    Add files via upload

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -5,13 +5,36 @@
 
 const int in1 = 32, in2 = 33, in3 = 25, in4 = 26; 
 const int stepsPerRevolution = 2048;
-Servo cameraServo, headServo;
 Stepper myStepper(stepsPerRevolution, 27, 12, 14, 13);
+
+Servo cameraServo, headServo;
+int headAngle = 90, cameraAngle = 20;
 
 RF24 rf24(17,5);
 const byte addr[] = "1Node";
+int beforeTime = 0, waitTime = 100;
+
+boolean isReload = false, isCharge = false;
 
 void setup() {
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
+    pinMode(2, OUTPUT); //LED comm status
+    pinMode(22, OUTPUT); //relay
+    pinMode(21, OUTPUT); //charge control
+    pinMode(15, OUTPUT); //fire control
+
+    digitalWrite(2, 0); //LED
+    digitalWrite(in1, 0);
+    digitalWrite(in2, 0);
+    digitalWrite(in3, 0);
+    digitalWrite(in4, 0);
+    digitalWrite(22, 1);
+    digitalWrite(21, 0);
+    digitalWrite(15, 0);
+
     Serial.begin(115200);
     rf24.begin();
     rf24.setChannel(80);  // 設定頻道編號
@@ -21,37 +44,34 @@ void setup() {
     rf24.startListening();  // 開始監聽無線廣播
     Serial.println("nRF24L01 ready!");
 
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-    pinMode(2, OUTPUT); //LED comm status
-
-    digitalWrite(2, 0); //LED
-    digitalWrite(in1, 0);
-    digitalWrite(in2, 0);
-    digitalWrite(in3, 0);
-    digitalWrite(in4, 0);
-
     cameraServo.attach(16);
-    cameraServo.write(0);
-    delay(100);
+    cameraServo.write(20);
+    delay(200);
     cameraServo.detach();
     headServo.attach(4);
-    headServo.write(0);
-    delay(100);
+    headServo.write(90);
+    delay(200);
     headServo.detach();
-
     myStepper.setSpeed(6);
 }
 
 void loop() {
-  if (rf24.available()) {
+    if(rf24.available()) {
         char msg[32];
         rf24.read(&msg, sizeof(msg));
         Serial.println(msg); // 顯示訊息內容
-        //operateCommand(msg);
+        operateCommand(msg);
+        beforeTime = millis();
     }
+    if(millis()-beforeTime >= waitTime){
+        digitalWrite(2, 0);
+        leftStop();
+        rightStop();
+        Serial.println("disconnect");
+    }else{
+        digitalWrite(2, 1);
+    }
+
 }
 
 void operateCommand(char msg[32]){
@@ -83,17 +103,43 @@ void operateCommand(char msg[32]){
         leftStop();
         rightStop();
     }else if(strcmp(msg,"cameraUp") == 0){
-        
+        if(cameraAngle>=55) return;
+        cameraServo.attach(16);
+        cameraServo.write(++cameraAngle);
+        delay(10);
+        cameraServo.detach();
     }else if(strcmp(msg,"cameraDown") == 0){
-        
-    }else if(strcmp(msg,"cameraStop") == 0){
-        
+        if(cameraAngle<=20) return;
+        cameraServo.attach(16);
+        cameraServo.write(--cameraAngle);
+        delay(10);
+        cameraServo.detach();
+    }else if(strcmp(msg,"headLeft") == 0){
+        if(headAngle>150) return;
+        headServo.attach(4);
+        headServo.write(++headAngle);
+        delay(10);
+        headServo.detach();
+    }else if(strcmp(msg,"headRight") == 0){
+        if(headAngle<30) return;
+        headServo.attach(4);
+        headServo.write(--headAngle);
+        delay(10);
+        headServo.detach();
     }else if(strcmp(msg,"leftRotate") == 0){
-        myStepper.step(32);
+        myStepper.step(8);
     }else if(strcmp(msg,"rightRotate") == 0){
-        myStepper.step(-32);
-    }else if(strcmp(msg,"startComm") == 0){
+        myStepper.step(-8);
+    }else if(strcmp(msg,"commCheck") == 0){
         digitalWrite(2, 1);
+    }else if(strcmp(msg,"fire") == 0){
+        leftStop();
+        rightStop();
+        delay(200);
+
+        reload();
+        recharge();
+        fire();
     }
 }
 
@@ -119,5 +165,46 @@ void rightDown(){
 }
 void rightStop(){
     digitalWrite(in3, 0);
-    digitalWrite(in4, 1);
+    digitalWrite(in4, 0);
+}
+void reload(){
+    Serial.println("reload");
+    cameraServo.attach(16);
+    cameraServo.write(20);
+    delay(150);
+    myStepper.step(stepsPerRevolution/32);
+    delay(500);
+    cameraServo.write(cameraAngle);
+    delay(200);
+    cameraServo.detach();
+    isReload = true;
+}
+void recharge(){ 
+    Serial.println("recharge");
+    digitalWrite(22, 0);//relay on
+    delay(1000);
+    digitalWrite(21, 1); //start charge
+    delay(100);
+    digitalWrite(21, 0);
+    delay(3000); //waiting charge
+    
+    isCharge = true;
+    delay(100);
+}
+void fire(){
+    if(!(isCharge && isReload)) return;
+    Serial.println("fire");
+    digitalWrite(15,1); //fire
+    delay(40);
+    digitalWrite(15,0);
+
+    delay(200); //discharge 
+    digitalWrite(15,1);
+    delay(300);
+    digitalWrite(15,0);
+
+    digitalWrite(22, 1); //relay off
+    isCharge = false;
+    isReload = false;
+    delay(100);
 }
--- arduino/receiver/receiver.ino
+++ arduino/receiver/receiver.ino
@@ -15,6 +15,9 @@ enum YState yState;
 short cameraAngleCounter = 0;
 short headAngleCounter = 0;
 
+double beforeTime = 0, fireWaitTime = 300, lastFireTime = 0, coldTime = 200;
+bool isFireBtnClick = false;
+
 void getDirection();
 void sendDirection();
 void sendNRFData();
@@ -27,7 +30,7 @@ void setup() {
     rf24.setPALevel(RF24_PA_MIN);
     rf24.setDataRate(RF24_2MBPS);
     rf24.stopListening();       // 停止偵聽；設定成發射模式
-    sendNRFData("startComm");
+    sendNRFData("commCheck");
 
     pinMode(2,INPUT);
     pinMode(3,INPUT);
@@ -46,33 +49,46 @@ void loop() {
 
     if(digitalRead(2) == 0){
         cameraAngleCounter++;
-        if(cameraAngleCounter%10 == 0) sendNRFData("cameraUp");
+        if(cameraAngleCounter%20 == 0) sendNRFData("cameraUp");
     }
     if(digitalRead(3)== 0){
         headAngleCounter--;
-        if(headAngleCounter%10 == 0) sendNRFData("headRight");
+        if(headAngleCounter%15 == 0) sendNRFData("headRight");
     }
     if(digitalRead(4)== 0){
         cameraAngleCounter--;
-        if(cameraAngleCounter%10 == 0) sendNRFData("cameraDown");
+        if(cameraAngleCounter%20 == 0) sendNRFData("cameraDown");
     }
     if(digitalRead(5)== 0){
         headAngleCounter++;
-        if(headAngleCounter%10 == 0) sendNRFData("headLeft");
+        if(headAngleCounter%15 == 0) sendNRFData("headLeft");
     }
     if(digitalRead(6)== 0) sendNRFData("leftRotate");
     if(digitalRead(7)== 0) sendNRFData("rightRotate");
-    if(digitalRead(8)== 0) Serial.println("main btn");
+
+    if(digitalRead(8)== 0 && isFireBtnClick == false){
+        beforeTime = millis();
+        isFireBtnClick = true;
+    }else if(digitalRead(8)== 1){
+        isFireBtnClick = false;
+    }else if(isFireBtnClick == true && millis()-beforeTime>=fireWaitTime && millis()-lastFireTime >= coldTime){
+        sendNRFData("fire");
+        lastFireTime = millis();
+        isFireBtnClick = false;
+    } 
 }
 
 void getDirection(){
-  if(analogRead(A0)>900) xState = right;
-  else if(analogRead(A0)<250) xState = left;
+  if(analogRead(A0)>1000) xState = left;
+  else if(analogRead(A0)<20) xState = right;
   else xState = normalX;
 
-  if(analogRead(A1)>800) yState = front;
-  else if(analogRead(A1)<250) yState = back;
+  if(analogRead(A1)>1000) yState = front;
+  else if(analogRead(A1)<320) yState = back;
   else yState = normalY;
+  Serial.print(analogRead(A0));
+  Serial.print(" ");
+  Serial.println(analogRead(A1));
 }
 
 void sendDirection(){
